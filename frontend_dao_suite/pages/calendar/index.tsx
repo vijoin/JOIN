b9af0fc70dc3b162afd6a685b0ab299243ae1a76
Sebas <sheva3232@gmail.com>
2023-06-04 10:23:21 -0500

    adding calendar working and style

--- frontend_dao_suite/pages/calendar/index.tsx
+++ frontend_dao_suite/pages/calendar/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import styled from "@emotion/styled";
+import EventModal from "../../components/modals/eventModal";
 
 const index = () => {
+  const [modalEvent, setModalEvent] = useState(false);
+  const onDateClick = (arg: any) => {
+    console.log(arg);
+    setModalEvent(true);
+  };
+  const closeModal = () => {
+    setModalEvent(false);
+  };
   return (
-    <div>index</div>
-  )
-}
+    <Container>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          { title: "event 1", date: "2023-06-04" },
+          { title: "event 2", date: "2023-06-06" },
+        ]}
+        dateClick={onDateClick}
+        height={"auto"}
+      />
+       <EventModal 
+        isOpen={modalEvent}
+        onClose={closeModal}
+       />
+    </Container>
+  );
+};
 
-export default index
+export default index;
+
+const Container = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

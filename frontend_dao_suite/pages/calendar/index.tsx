d7b46f0363972ae9450f034adce5bee9b751248e
Sebas <sheva3232@gmail.com>
2023-06-05 16:07:24 -0500

    interacting with polybase

--- frontend_dao_suite/pages/calendar/index.tsx
+++ frontend_dao_suite/pages/calendar/index.tsx
@@ -1,11 +1,31 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import styled from "@emotion/styled";
 import EventModal from "../../components/modals/EventModal";
+import { Polybase } from "@polybase/client";
+import "dotenv/config";
 
 const index = () => {
+  const db = new Polybase({
+    defaultNamespace: "pk/0xb5f85f82153b100b238a20c23297acc9d38f91b3aab0dcd5722b1b870433ef3b6baef2a0d70970bfe49e45d8e29e4ea8edb5638b6d06a49db9957ccc4775a7d5/DAOSuite",
+  });
+
+  useEffect(() => {
+    readData();
+  }, []);
+  const readData = async () => {
+    try {
+      const  {data}  = await db.collection("Tag").record("dao").get();
+      console.log(data);
+      console.log("Tag id:", data.id);
+      console.log("Tag name:", data.name);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const [modalEvent, setModalEvent] = useState(false);
   const onDateClick = (arg: any) => {
     console.log(arg);
@@ -14,6 +34,7 @@ const index = () => {
   const closeModal = () => {
     setModalEvent(false);
   };
+
   return (
     <Container>
       <FullCalendar
@@ -26,10 +47,7 @@ const index = () => {
         dateClick={onDateClick}
         height={"auto"}
       />
-       <EventModal 
-        isOpen={modalEvent}
-        onClose={closeModal}
-       />
+      <EventModal isOpen={modalEvent} onClose={closeModal} />
     </Container>
   );
 };

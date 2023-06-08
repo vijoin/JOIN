fbd1dfc3419552e4b4aeb2c4ae569f1563be9bc7
gitguti <gitmelgtrrz1@gmail.com>
2023-06-08 06:45:29 -0400

    created sidebar component

--- frontend_dao_suite/pages/index.tsx
+++ frontend_dao_suite/pages/index.tsx
@@ -1,17 +1,22 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import PageLayout from '../layouts/PageLayout';
-import Hero from '../components/Hero';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+// import Head from 'next/head';
+import styles from "../styles/Home.module.css";
+import PageLayout from "../layouts/PageLayout";
+// import Hero from '../components/Hero';
+import { Grid, GridItem } from "@chakra-ui/react";
+import { SidebarFilters } from "../components/SidebarFilters";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <PageLayout
-        title='Home'
-        footer={true}
-      >
-        <Hero />
+      <PageLayout title="Home" footer={true}>
+        {/* <Hero /> */}
+        <Grid templateColumns="repeat(8, 1fr)">
+          <GridItem colSpan={1} bg="tomato">
+            <SidebarFilters />
+          </GridItem>
+          <GridItem colSpan={7} bg="papayawhip" />
+        </Grid>
       </PageLayout>
     </div>
   );

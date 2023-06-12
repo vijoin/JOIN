4aa846c5dfb8917f3800bc659e141cc0a6c23ce1
gitguti <gitmelgtrrz1@gmail.com>
2023-06-12 01:30:39 -0400

    updated layout

--- frontend_dao_suite/layouts/PageLayout.tsx
+++ frontend_dao_suite/layouts/PageLayout.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { ReactNode } from "react";
-
+// components
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+//styles
+import { SidebarFilters } from "../components/Sidebar";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 type Props = {
   children: JSX.Element | JSX.Element[] | ReactNode;
@@ -17,11 +20,32 @@ const PageLayout = ({ title, children, footer }: Props) => {
       <Head>
         <title>{pageTitle}</title>
       </Head>
-      <Header />
-      <div className="flex flex-col min-h-screen">
-        <main>{children}</main>
-      </div>
-      {footer && <Footer />}
+      <Grid
+        templateAreas={`"nav header header"
+                  "nav main event"
+                  "footer footer footer"`}
+        gridTemplateRows={"50px 1fr 30px"}
+        gridTemplateColumns={"250px 1fr 350px"}
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" area={"header"}>
+          <Header />
+        </GridItem>
+        <GridItem area={"nav"}>
+          <SidebarFilters />
+        </GridItem>
+        <GridItem area={"main"}>
+          <div className="flex flex-col min-h-screen">
+            <main>{children}</main>
+          </div>
+        </GridItem>
+        <GridItem area={"event"}>
+          <SidebarFilters />
+        </GridItem>
+        <GridItem area={"footer"}>{footer && <Footer />}</GridItem>
+      </Grid>
     </>
   );
 };
--- frontend_dao_suite/pages/index.tsx
+++ frontend_dao_suite/pages/index.tsx
@@ -1,31 +1,21 @@
 import type { NextPage } from "next";
-// import Hero from '../components/Hero';
 //styles
 import styles from "../styles/Home.module.css";
-import { Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 // components
 import PageLayout from "../layouts/PageLayout";
-import { SidebarFilters } from "../components/Sidebar";
 import  Card from "../components/Card";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <PageLayout title="Home" footer={true}>
-        {/* <Hero /> */}
-        <Grid templateColumns="repeat(8, 1fr)">
-          <GridItem colSpan={1} bg="tomato">
-            <SidebarFilters />
-          </GridItem>
-          <GridItem colSpan={7} px={6} py={8}>
-          <SimpleGrid  minChildWidth='120px' spacing='20px'>
+          <SimpleGrid  minChildWidth='300px' spacing='20px'>
             <Card/>
             <Card/>
             <Card/>
             <Card/>
           </SimpleGrid>
-          </GridItem>
-        </Grid>
       </PageLayout>
     </div>
   );

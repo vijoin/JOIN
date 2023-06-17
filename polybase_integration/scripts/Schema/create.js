d8bd9bf37b0d40d8bce22e5543622010d801a91d
Victor Inojosa <vijoin@gmail.com>
2023-06-16 23:52:16 -0300

    [backend] Allow reminders to be marked as Sent

--- polybase_integration/scripts/Schema/create.js
+++ polybase_integration/scripts/Schema/create.js
@@ -144,12 +144,18 @@ collection ReminderEventSubscriber {
     subscriber: string;
     event: Event;
     timestamp: number;
+    state: string;
 
     constructor (id: string, subscriber: string, event: Event, timestamp: number) {
         this.id = id;
         this.subscriber = subscriber;
         this.event = event;
         this.timestamp = timestamp;
+        this.state = 'pending';
+    }
+
+    setSent () {
+      this.state = 'sent';
     }
 
     del () {
